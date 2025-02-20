61212cd8dc9d0ab810987e29af94a97eb3d5cae0
Rokibul Alom <itismehey975@gmail.com>
2025-02-20 22:42:36 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+           <h1 className=" font-bold text-6xl text-center">Tailwind is working</h1>
+        </div>
+    );
+};
+
+export default Home;
